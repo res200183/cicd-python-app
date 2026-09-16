2914349be5d3598584357ad9060d9d5c46b11a9d
Oleksander Sychov <113937544+res200183@users.noreply.github.com>
2026-09-16 13:33:57 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,4 +6,4 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello from CI/CD!"
+    assert response.data == b"Hello from automatic CI/CD deployment!"
